2277fdde632c0bb5a4b795d2e6826f713aa50fac
matt <matt@cantab.net>
2013-09-02 11:27:24 +0100

    Added a hand parser

---
+++ hand.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+ranks = '23456789TJQKA'
+
+def ridx(r):
+  return ranks.index(r)
+
+class Hand(object):
+  cards = []
+
+  def __init__(self, s):
+    self.cards = self.parse(s)
+
+  def parse(self, s):
+    suited = False
+    suit = 0
+    ret = []
+
+    for c in s:
+      if c == '(':
+        suited = True
+      elif c == ')':
+        suited = False
+        suit += 1
+      else:
+        rank = ridx(c)
+        card = (rank, suit)
+        ret.append(card)
+
+        if not suited:
+          suit += 1
+
+    return ret
